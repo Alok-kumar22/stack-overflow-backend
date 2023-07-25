10eaa75c71f5841863cb7354631cd1f880c03c72
dishu001 <2020eeb1167@iitrpr.ac.in>
2023-07-25 12:50:06 +0530

    first commit

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Razorpay from "razorpay";
+import connectDB from "./config/db.js";
+import UserRoutes from "./routes/users.js";
+import QuestionRoutes from "./routes/Question.js";
+import answerRoutes from "./routes/Answer.js";
+import postRouter from "./routes/posts.js";
+import cloudinary from "cloudinary";
+import serverless from "serverless-http"
+
+
+const router = express.Router();
+connectDB();
+
+cloudinary.v2.config({
+    cloud_name: "dzf2bn5ws",
+    api_key: "976119987757764",
+    api_secret: "***************************",
+});
+
+const app = express();
+dotenv.config();
+app.use(express.json({ limit: "30mb", extended: true }));
+
+app.use(cors());
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+export const instance = new Razorpay({
+    key_id: "rzp_test_sgdK1uHHwrzcFi",
+    key_secret: "X9tCCmCavSYEQ1GhYKFvn75K",
+});
+router.use("/user", UserRoutes);
+router.use("/questions", QuestionRoutes);
+router.use("/answer", answerRoutes);
+router.use("/post", postRouter);
+
+
+// app.listen(5000, () => {
+//     console.log("Server run at Port 5000");
+// });
+
+router.get("/", (req, res) => {
+    res.json({
+        message: "Hello, this is the welcome page.",
+        path: "/",
+    });
+});
+
+app.use("/.netlify/functions/index", router);
+
+
+
+export const handler = serverless(app);
