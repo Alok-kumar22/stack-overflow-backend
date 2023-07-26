694925a74f4a15d5cf7443e5cc7df1e82253adf2
Alok-kumar22 <92496546+Alok-kumar22@users.noreply.github.com>
2023-07-26 14:28:48 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ const app = express();
 const router = express.Router();
 dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }));
-app.use(express.urlencoded({ limit: "30mb", extended: false }));
+// app.use(express.urlencoded({ limit: "30mb", extended: false }));
 app.use(cors());
 
 cloudinary.v2.config({
